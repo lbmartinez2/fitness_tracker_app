8783cb3c3d021b698ec5d3cd08ebb417384d1515
Lindsay Joseph Martinez <mlindsayjoseph@gmail.com>
2024-07-30 18:20:59 +0800

    completed user sign-in

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_29_123912) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_30_095848) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "consumptions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_consumptions_on_user_id"
   end
 
   create_table "exercises", force: :cascade do |t|
@@ -55,5 +57,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_29_123912) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "consumptions", "users"
   add_foreign_key "exercises", "users"
 end
