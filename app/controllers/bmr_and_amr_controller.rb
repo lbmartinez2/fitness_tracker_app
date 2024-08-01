05af47e7401e5f292b8dd9101f61cfcdba554703
Anst09 <anp.obras97@gmail.com>
2024-08-01 18:29:30 +0800

    corrected my calculator controller

--- app/controllers/bmr_and_amr_controller.rb
+++ app/controllers/bmr_and_amr_controller.rb
@@ -1,2 +1,31 @@
 class BmRandAmrController < ApplicationController
-end
+    def new
+      # This method can render a form to input the necessary data
+    end
+  
+    def calculate
+      weight = params[:weight].to_f
+      height = params[:height].to_f
+      age = params[:age].to_f
+      gender = params[:gender]
+      activity_level_factor = params[:activity_level_factor].to_f
+  
+      # Calculate BMR
+      bmr = if gender == 'male'
+              88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+            else
+              447.593 + (9.247 * weight) + (3.096 * height) - (4.330 * age)
+            end
+  
+      # Calculate AMR
+      amr = bmr * activity_level_factor
+  
+      @result = { bmr: bmr, amr: amr }
+  
+      respond_to do |format|
+        format.html { render :new } # Assuming you want to render the new form with the result
+        format.json { render json: @result }
+      end
+    end
+  end
+  
