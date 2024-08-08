7473bca2155c94db8c557d7f800e884da8a92e50
Anst09 <anp.obras97@gmail.com>
2024-08-08 23:11:37 +0800

    tweaked the calculator

--- app/controllers/bmr_and_amr_controller.rb
+++ app/controllers/bmr_and_amr_controller.rb
@@ -1,20 +1,21 @@
 class BmrAndAmrController < ApplicationController
-    before_action :authenticate_user!
-  
-    def new
-      # Initialize BMR and AMR to nil to avoid errors when the form is initially loaded
-      @bmr = nil
-      @amr = nil
-    end
-  
-    def calculate
-     # Update the user's weight in the database
-      if current_user.update(weight: params[:weight])
+  before_action :authenticate_user!
+
+  def new
+    # Initialize BMR and AMR to nil to avoid errors when the form is initially loaded
+    @bmr = nil
+    @amr = nil
+  end
+
+  def calculate
+    if current_user.update(weight: params[:weight])
       weight = current_user.weight
       height = current_user.height
       age = current_user.age
-      gender = current_user.sex
-      activity_level = current_user.current_activity_level
+      gender = params[:gender] || current_user.sex
+      activity_level = params[:activity_level] || current_user.current_activity_level
+  
+      Rails.logger.debug "Weight: #{weight}, Height: #{height}, Age: #{age}, Gender: #{gender}, Activity Level: #{activity_level}"
   
       # Calculate BMR
       bmr = if gender == 'male'
@@ -23,31 +24,36 @@ class BmrAndAmrController < ApplicationController
               447.593 + (9.247 * weight) + (3.096 * height) - (4.330 * age)
             end
   
-
-      # Calculate AMR
-      amr = bmr * activity_level_factor
-
-      # Calculate AMR using switch-case for activity levels 
-      @amr = case activity_level
-             when 'Sedentary'
-               @bmr * 1.2
-             when 'Low Active'
-               @bmr * 1.375
-             when 'Active'
-               @bmr * 1.55
-             when 'Very Active'
-               @bmr * 1.725
-             else
-               @bmr # Default to BMR if activity level is not recognized
-             end
-
+      Rails.logger.debug "Calculated BMR: #{bmr}"
   
-      @result = { bmr: bmr, amr: amr }
+      # Calculate AMR using switch-case for activity levels
+      amr = case activity_level
+            when 'Sedentary'
+              bmr * 1.2
+            when 'Low Active'
+              bmr * 1.375
+            when 'Moderately Active'
+              bmr * 1.55
+            when 'Active'
+              bmr * 1.725
+            when 'Very Active'
+              bmr * 1.9
+            else
+              bmr # Default to BMR if activity level is not recognized
+            end
+  
+      Rails.logger.debug "Calculated AMR: #{amr}"
+  
+      @bmr = bmr.round
+      @amr = amr.round
   
       respond_to do |format|
-        format.html { render :new } # Assuming you want to render the new form with the result
-        format.json { render json: @result }
+        format.html { render :new } # Render the form with the result
+        format.json { render json: { bmr: @bmr, amr: @amr } }
       end
+    else
+      flash[:error] = "Failed to update weight."
+      render :new
     end
   end
-  
+end
