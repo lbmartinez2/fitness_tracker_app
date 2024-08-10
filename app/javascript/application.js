3b201504e5e9e5e84bfec356073191a03cd52667
lbmartinez2 <155975590+lbmartinez2@users.noreply.github.com>
2024-08-10 10:33:28 +0800

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require rails-ujs
+
 //= require echarts.min.js
 //= require echarts/theme/dark.js
 //= require echarts
